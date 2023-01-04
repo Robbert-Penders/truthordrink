a1c5084b59eace4d237274776f24ed77bbdab7ab
Robbert Penders <1940406penders@zuyd.nl>
2023-01-04 12:35:56 +0100

    Detect shake class toegevoegd voor duidelijkheid

--- truthordrink/truthordrink.Android/MainActivity.cs
+++ truthordrink/truthordrink.Android/MainActivity.cs
@@ -11,6 +11,7 @@ namespace truthordrink.Droid
     [Activity(Label = "truthordrink", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
---
+++ truthordrink/truthordrink/Model/DetectShake.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace truthordrink.Model
+{
+    public class DetectShake
+    {
+        // Set speed delay for monitoring changes.
+        SensorSpeed speed = SensorSpeed.Game;
+
+        public DetectShake()
+        {
+            // Register for reading changes, be sure to unsubscribe when finished
+            Accelerometer.ShakeDetected += Accelerometer_ShakeDetected;
+        }
+
+        void Accelerometer_ShakeDetected(object sender, EventArgs e)
+        {
+            // Process shake event
+        }
+
+        public void ToggleAccelerometer()
+        {
+            try
+            {
+                if (Accelerometer.IsMonitoring)
+                    Accelerometer.Stop();
+                else
+                    Accelerometer.Start(speed);
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature not supported on device
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
+        }
+    }
+}
